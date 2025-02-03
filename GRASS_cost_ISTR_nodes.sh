5b6a1f446f45065df371a97b727bd9a26640be18
Andrew McLean <73619434+amclean6787@users.noreply.github.com>
2025-02-03 15:25:27 +0100

    Added GRASS code

---
+++ GRASS_cost_ISTR_nodes.sh
@@ -0,0 +1,14 @@
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Parecag_30m@PERMANENT start_coordinates=274149.119857447,5043282.10123474
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Dragonja_30m@PERMANENT start_coordinates=278370.297060218,5040106.36061736
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Humagum_30m@PERMANENT start_coordinates=267195.014651279,5036701.0622217
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Emonia_30m@PERMANENT start_coordinates=270072.758004166,5024192.15698098
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Ad_Ningum_30m@PERMANENT start_coordinates=283353.916500555,5027829.39278541
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Parentium_30m@PERMANENT start_coordinates=271926.413814554,5013757.74879091
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Pola_30m@PERMANENT start_coordinates=290244.502495666,4973416.34456122
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Nesactium_30m@PERMANENT start_coordinates=300263.874047165,4978284.20381187
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Arsia_fl__30m@PERMANENT start_coordinates=308238.775019712,4984997.91984747
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Albona_30m@PERMANENT start_coordinates=313550.914559902,4996591.89897111
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Flanona_30m@PERMANENT start_coordinates=318439.994148352,5002522.68710231
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Castra_30m@PERMANENT start_coordinates=331826.274213968,5028284.21842074
+r.cost -k --overwrite input=ISTR_Time_30m@PERMANENT output=ISTR_CmCost_Ruginium_30m@PERMANENT start_coordinates=274782.37303566,4997406.01416018
+echo "Finished processing"
