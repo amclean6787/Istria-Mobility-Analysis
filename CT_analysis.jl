a28732f74a469a3956e6c380375ec7f611cf838f
Andrew McLean <73619434+amclean6787@users.noreply.github.com>
2025-02-06 13:31:10 +0100

    Julia circuitscape code examples

---
+++ CT_analysis.jl
@@ -0,0 +1,8 @@
+using Circuitscape
+
+compute("Data/Input/CT/Config/INI/Nodes_pairwise.ini")
+
+compute("Data/Input/CT/Config/INI/Directional/east-west_seaav.ini")
+compute("Data/Input/CT/Config/INI/Directional/north-south_seaav.ini")
+compute("Data/Input/CT/Config/INI/Directional/south-north_seaav.ini")
+compute("Data/Input/CT/Config/INI/Directional/west-east_seaav.ini")
